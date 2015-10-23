5345ee25bca5d8024641245737b61ba58d88d252
Tingting Jiang <tvj@vt.edu>
2015-10-23 20:50:45 +0000

    Batch assign DOIs with checkboxes in pending requests page

--- app/controllers/doi_requests_controller.rb
+++ app/controllers/doi_requests_controller.rb
@@ -3,26 +3,22 @@ class DoiRequestsController < ApplicationController
   load_and_authorize_resource except: [:create]
 
   def index
-    @doi_requests = DoiRequest.sorted
+    @doi_requests = DoiRequest.sorted.page(params[:page]).per(10)
   end
 
   def pending
-    @doi_requests = DoiRequest.pending
-    render('index')
+    @doi_requests = DoiRequest.pending.page(params[:page]).per(10)
   end
 
   def create
-    klass = Object.const_get params[:asset_type]
-    @asset = klass.find(params[:asset_id])
+    # For now, assume the asset_type is Collection only
+    # if params[:asset_type] == "Collection"
+    @asset = Collection.find(params[:asset_id])
     @asset[:identifier] << t('doi.pending_doi')
     doi_request = DoiRequest.new(asset_id: params[:asset_id], asset_type: params[:asset_type])
     if doi_request.save && @asset.update_attributes({:identifier => @asset[:identifier]})
       flash[:notice] = t('doi.messages.submit.success')
-      if klass == Collection
-        redirect_to collections.collection_path(@asset)
-      else # TODO: redirect to the other asset type show page
-        redirect_to doi_requests_path
-      end
+      redirect_to collections.collection_path(@asset)
     else
       flash[:error] = t('doi.messages.submit.failure')
       redirect_to doi_requests_path
@@ -30,40 +26,53 @@ class DoiRequestsController < ApplicationController
   end
 
   def mint_doi
-    @doi_request = DoiRequest.find(params[:id])
-
-    if @doi_request.collection?
-      @collection = Collection.find(@doi_request.asset_id)
-      minted_doi = Ezid::Identifier.create(
-      	datacite_creator: (@collection.creator.empty? ? "" : @collection.creator.first), 
-        datacite_resourcetype: "Dataset",
-        datacite_title: @collection.title,
-        datacite_publisher: (@collection.publisher.empty? ? "" : @collection.publisher.first), 
-        datacite_publicationyear: (@collection.date_created.empty? ? "" : @collection.date_created.first)
-         )
-      @collection[:identifier].each_with_index {
-        |id, idx| id == t('doi.pending_doi') ? @collection[:identifier][idx] = minted_doi.id : id
-      }
-      if @doi_request.update_attributes({:ezid_doi => minted_doi.id}) && 
-        @collection.update_attributes({:identifier => @collection[:identifier]})
-        flash[:notice] = t('doi.messages.mint.success')
-        redirect_to collections.collection_path(@doi_request.asset_id)
-      else
-        flash[:error] = t('doi.messages.mint.failure')
-        redirect_to doi_requests_path
-      end
+    doi_request = DoiRequest.find(params[:id])
+    if mint_collection_doi(doi_request)
+      flash[:notice] = t('doi.messages.mint.success')
     else
-      redirect_to doi_requests_path
+      flash[:error] = t('doi.messages.mint.failure')
     end
+    redirect_to doi_requests_path
   end
 
   def mint_all
+    mint_success = true
+    params[:doi_requests_checkbox].each do |id|
+      doi_request = DoiRequest.find(id)
+      if !mint_collection_doi(doi_request)
+        mint_success = false
+      end
+    end
+    if mint_success
+      flash[:notice] = "Batch mint complete."
+    else
+      flash[:error] = "Error in batch mint."
+    end
+    redirect_to doi_requests_path
   end
 
   def view_doi
     @doi_request = DoiRequest.find(params[:id])
     @ezid_doi = Ezid::Identifier.find(@doi_request.ezid_doi)
   end
+
+  private
+
+    def mint_collection_doi(doi_request)
+      asset = Collection.find(doi_request.asset_id)
+      minted_doi = Ezid::Identifier.create(
+        datacite_creator: (asset.creator.empty? ? "" : asset.creator.first), 
+        datacite_resourcetype: "Dataset",
+        datacite_title: asset.title,
+        datacite_publisher: (asset.publisher.empty? ? "" : asset.publisher.first), 
+        datacite_publicationyear: (asset.date_created.empty? ? "" : asset.date_created.first)
+       )
+      asset[:identifier].each_with_index {
+        |id, idx| id == t('doi.pending_doi') ? asset[:identifier][idx] = minted_doi.id : id
+      }  
+      return doi_request.update_attributes({:ezid_doi => minted_doi.id}) && 
+        asset.update_attributes({:identifier => asset[:identifier]})
+    end
 
 end
 
