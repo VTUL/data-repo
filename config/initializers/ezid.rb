e1fc91817116c0f788c720491d1789ba9db330c4
Collin Brittle <cnbrittle@gmail.com>
2016-02-05 20:58:40 +0000

    Update config files to read settings from secrets.yml
    Also updates Travis to run with our new setup.

--- config/initializers/ezid.rb
+++ config/initializers/ezid.rb
@@ -4,9 +4,9 @@ Ezid::Client.configure do |config|
   #config.use_ssl = true
   #config.timeout = 600
   # VT libraries' unique DOI prefix with EZID
-  config.default_shoulder = "doi:10.5072/FK2"
-  config.user = "apitest"
-  config.password = "apitest"
+  config.default_shoulder = Rails.application.secrets['ezid']['default_shoulder']
+  config.user = Rails.application.secrets['ezid']['user']
+  config.password = Rails.application.secrets['ezid']['password']
   config.identifier.defaults[:status] = "reserved"
   config.identifier.defaults[:profile] = "datacite"
 end
