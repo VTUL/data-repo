f12898a4b05a6e00a759d5007f7734acf9502e1b
data-repo application owner <hydra@hydravm>
2017-05-31 14:04:32 +0000

    auth controller

--- app/controllers/osf_auth_controller.rb
+++ app/controllers/osf_auth_controller.rb
@@ -1,13 +1,50 @@
 class OsfAuthController < ApplicationController
+before_action :get_client
+
   def index
   end
 
   def auth
-  end
-
-  def callback
+    auth_url = @client.auth_code.authorize_url(
+      :redirect_uri => callback_url,
+      :scope => 'osf.full_read',
+      :response_type => 'code',
+      :state => 'iuasdhf734t9hiwlf7'
+    )
+    redirect_to auth_url
   end
 
   def token
   end
+
+  def callback
+    code = params['code']
+    if !code.blank?
+      oauth_token = @client.auth_code.get_token(code, :redirect_uri => callback_url)
+      if !oauth_token.blank?
+        session['oauth_token'] = oauth_token
+        redirect_to api_list_url 
+      end
+    end
+  end
+
+  def callback_url
+    "#{request.base_url}/oauth2/callback"
+  end
+
+  def get_client
+    @client ||=  OAuth2::Client.new(
+      Rails.application.secrets['osf_client_id'],
+      Rails.application.secrets['osf_client_secret'],
+      :site => Rails.application.config.osf_auth_site,
+      :authorize_url=> Rails.application.config.osf_authorize_url,
+      :token_url=> Rails.application.config.osf_token_url
+    )
+  end
+
+  def oauth_token
+    @oauth_token = OAuth2::AccessToken.from_hash(get_client, session['oauth_token'])
+  end
+
 end
+
