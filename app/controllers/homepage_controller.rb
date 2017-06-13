cf2e2c4f4713b88da4058babb3b3255523868dd2
Lee Hunter <whunter@vt.edu>
2017-06-13 18:31:47 +0000

    initial header styling. add new logo
    fix image
    
    add right column and begin publish within it
    
    move right col into a partial
    
    add curation services block and refactor css a little
    
    move login button. remove search from header
    
    move search down. add left-col text. add Univ Lib header
    
    add purpose statement
    
    working on the header stuff
    
    position login/user_util links. fix right col links. adjust text size and search positioning in left col. add centering div and logo title row to other layouts.
    
    add policy links to right column
    
    re-style footer
    
    refactor carousel to match new comps
    
    fix 2 column section on homepage that was being pushed down
    
    add search bar to masthead on all pages except home
    
    move homepage right column blocks into partials. add route_exists helper method to homepage controller. only render policy page links if policy page exists.
    
    remove rotating carousel. one banner image, randomly chosen
    
    refactor image shuffle and select so that it isn't limited to 4 images
    
    fix exception that's thrown when there are no carousel images present
    
    update copy. move publish to left
    
    moar styling!
    
    make login larger
    
    move user_util_links up in zindex
    
    fix app controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,26 @@
 class HomepageController < ApplicationController
   include Sufia::HomepageController
+  helper_method :route_exists
 
   def index
     super
 
     @featured_work = ContentBlock.find_or_create_by(name: ContentBlock::WORK)
 
-    @images = Pathname.glob(Rails.root.join('app/assets/images/carousel/*.[Jj][Pp][Gg]')).shuffle[0..3]
-    @images.map! { |img| img.relative_path_from(Rails.root.join('app/assets/images')).to_s }
+    images = Pathname.glob(Rails.root.join('app/assets/images/carousel/*.[Jj][Pp][Gg]')) 
+    if !images.empty?
+      @banner_image = images.shuffle[0..(images.length - 1)].first.relative_path_from(Rails.root.join('app/assets/images')).to_s
+    else
+      @banner_image = ""
+    end
+  end
+
+  def route_exists route
+    begin
+      recognized = Rails.application.routes.recognize_path(route)
+    rescue
+      recognized = false
+    end
+    return recognized
   end
 end
