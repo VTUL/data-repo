455496135221b9541cf555e0c11893dd383d6063
data-repo application owner <hydra@hydravm>
2017-10-02 21:00:13 +0000

    refactor meta begin
    add admin option to route and all items to admin_csv_stuff
    
    add admin to doi link and begin handling
    
    admin and reg user dataset downloads
    
    oops. needs to be == not =

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -10,6 +10,31 @@ class GenericFile < ActiveFedora::Base
     ['id', 'mime_type', 'file_size', 'filename', 'checksum', 'depositor', 'title', 'resource_type', 'keyword', 'creator', 'rights', 'contributor', 'abstract', 'date_created', 'subject', 'language', 'identifier', 'location', 'related_url', 'provenance', 'in_collections']
   end
 
+  def admin_csv_headers
+    admin_solr_headers.concat admin_fedora_headers
+  end
+
+  def admin_solr_headers
+    ["system_create_dtsi", "system_modified_dtsi", "active_fedora_model_ssi", "has_model_ssim", "object_profile_ssm", "mime_type_tesim", "depositor_ssim", "depositor_tesim", "resource_type_tesim", "resource_type_sim", "title_tesim", "title_sim", "creator_tesim", "creator_sim", "contributor_tesim", "contributor_sim", "description_tesim", "tag_tesim", "tag_sim", "rights_tesim", "date_created_tesim", "date_uploaded_dtsi", "date_modified_dtsi", "subject_tesim", "subject_sim", "language_tesim", "language_sim", "identifier_tesim", "based_near_tesim", "based_near_sim", "related_url_tesim", "isPartOf_ssim", "label_tesim", "file_format_tesim", "file_format_sim", "all_text_timv", "file_size_is", "digest_ssim", "read_access_group_ssim", "edit_access_person_ssim"]
+  end
+
+  def admin_fedora_headers
+    ["id", "mime_type", "format_label", "file_size", "last_modified", "filename", "original_checksum", "rights_basis", "copyright_basis", "copyright_note", "well_formed", "valid", "status_message", "file_title", "file_author", "page_count", "file_language", "word_count", "character_count", "paragraph_count", "line_count", "table_count", "graphics_count", "byte_order", "compression", "color_space", "profile_name", "profile_version", "orientation", "color_map", "image_producer", "capture_device", "scanning_software", "exif_version", "gps_timestamp", "latitude", "longitude", "character_set", "markup_basis", "markup_language", "bit_depth", "channels", "data_format", "offset", "frame_rate", "label", "depositor", "arkivo_checksum", "relative_path", "import_url", "part_of", "resource_type", "title", "creator", "contributor", "description", "tag", "rights", "date_created", "date_uploaded", "date_modified", "subject", "language", "identifier", "based_near", "related_url", "bibliographic_citation", "source", "proxy_depositor", "on_behalf_of", "provenance", "batch_id"]
+  end
+
+  def admin_csv_values
+    ret_array = []
+    admin_solr_headers.each do | key |
+      value = !self.to_solr[key].blank? ? self.to_solr[key] : ""
+      ret_array << (value.respond_to?("each") ? value.join("||") : value)
+    end
+    admin_fedora_headers.each do | key |
+      value = !self.send(key).blank? ? self.send(key) : ""
+      ret_array << (value.respond_to?("each") ? value.join("||") : value)
+    end
+    ret_array
+  end
+
   def csv_values
     ret_array = []
     collections = self.collections.map{ |c| c.id }.join('||') rescue ""
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'policies', to: 'pages#show', id: 'policies_page', as: 'policies'
   get 'user_guides', to: 'pages#show', id: 'user_guides_page', as: 'user_guides'
   get 'collections/:id/add-files', to: 'collections#add_files', as: 'collections_add_files'
-  get 'collections/:id/dataset-download', to: 'collections#dataset_download', as: 'collections_download'
+  get 'collections/:id/dataset-download(/:admin)', to: 'collections#dataset_download', as: 'collections_download'
   get 'dashboard/admin_metadata_download', to: 'dashboard#admin_metadata_download', as: 'admin_metadata_download'
 
   blacklight_for :catalog
