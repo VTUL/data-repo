b6d2e4cdb67cd1509c0a0c3afd5dace1868f6c43
data-repo application owner <hydra@hydravm>
2018-04-06 18:24:33 +0000

    use model.all instead of solr query

--- lib/vtech_data/download_generator.rb
+++ lib/vtech_data/download_generator.rb
@@ -29,19 +29,19 @@ class DownloadGenerator
   def generate_all_metadata class_name
     @model_name = class_name
     @my_class = class_name.constantize
-    items = get_all_items_for_model.map{ |item| item["id"] }
+    items = get_all_items_for_model(my_class)
     file_path = File.join(self.class.csv_dir, archive_name, "#{timestamp}_#{model_name.downcase}.csv")
     write_csv(file_path, my_class, items, true)
   end
 
   def generate_dataset_download dataset_id, admin_download
     collection_file_path = File.join(self.class.csv_dir, archive_name, "dataset_#{dataset_id}.csv")
-    write_csv(collection_file_path, Collection, [dataset_id], admin_download)
-    collection_item_ids = Collection.find(dataset_id).member_ids
-    if !collection_item_ids.blank?
+    write_csv(collection_file_path, Collection, [Collection.find(dataset_id)], admin_download)
+    collection_members = Collection.find(dataset_id).members
+    if !collection_members.blank?
       items_file_path = File.join(self.class.csv_dir, archive_name, "dataset_#{dataset_id}_items.csv")
-      write_csv(items_file_path, GenericFile, collection_item_ids, admin_download)
-      add_files(collection_item_ids)
+      write_csv(items_file_path, GenericFile, collection_members, admin_download)
+      add_files(collection_members)
     end
   end
 
@@ -58,26 +58,26 @@ class DownloadGenerator
 
   private
 
-    def get_all_items_for_model
-      query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: model_name)
-      ActiveFedora::SolrService.query(query)
+    def get_all_items_for_model item_class
+      item_class.all
+      #query = ActiveFedora::SolrQueryBuilder.construct_query_for_rel(has_model: model_name)
+      #ActiveFedora::SolrService.query(query)
     end
 
-    def add_files item_ids
-      item_ids.each do |item_id|
-        generic_file = GenericFile.find(item_id)
+    def add_files items
+      items.each do |generic_file|
         filename = !generic_file.filename.empty? ? generic_file.filename : generic_file.label
         item_target_path = File.join(archive_full_path, filename)
         # If a file already exists with this filename then put this one in a sub-directory named after the item id
         if File.file? item_target_path
-          FileUtils::mkdir_p(File.join(archive_full_path, item_id))
-          item_target_path =  File.join(archive_full_path, item_id, filename)
+          FileUtils::mkdir_p(File.join(archive_full_path, generic_file.id))
+          item_target_path =  File.join(archive_full_path, generic_file.id, filename)
         end
         File.open(item_target_path, 'wb') { |file| file.write(generic_file.content.content) }
       end      
     end
 
-    def write_csv file_path, item_class, item_ids, admin_download
+    def write_csv file_path, item_class, records, admin_download
       CSV.open(file_path,'wb') do |csv|
         if admin_download
           headers = item_class.new.admin_csv_headers
@@ -85,8 +85,7 @@ class DownloadGenerator
           headers = item_class.new.csv_headers
         end
         csv << headers
-        item_ids.each do |item_id|
-          record = item_class.find(item_id)
+        records.each do |record|
           if admin_download
             values = record.admin_csv_values
           else
@@ -98,3 +97,4 @@ class DownloadGenerator
     end
 
 end
+
