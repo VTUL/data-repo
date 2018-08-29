cbb9adbffb215dc7af86a9c1e009fd5c85c332d0
data-repo application owner <hydra@data-repo.dld.lib.vt.edu>
2018-08-29 17:27:01 +0000

    track itemdownload from list

--- app/assets/javascripts/datarepo/analytics.js
+++ app/assets/javascripts/datarepo/analytics.js
@@ -1,6 +1,26 @@
+AnalyticsHelper = {
+  pageTracker: null,
+  analyticsID: null,
+  
+  init: function(analyticsID) {
+    this.analyticsID = analyticsID;
+  },
+
+  getTracker: function() {
+    if(this.pageTracker == null) {
+      this.pageTracker = _gat._getTracker(this.analyticsID);
+    }
+  },
+  
+  setListeners: function() {
+    var _this = this;
+    $('.itemdownload').click(function(){
+      _this.getTracker();
+      var itemID = $(event.target).attr('href').replace('/downloads/', '');
+      _this.pageTracker._trackEvent('Items','Download', itemID);
+    });
+  }
+}
 Blacklight.onLoad(function() {
-  $('.itemdownload').click(function(){
-    event.preventDefault();
-    var itemID = $(event.target).attr('href').replace('/downloads/', '');
-  }); 
+  AnalyticsHelper.setListeners();
 });
