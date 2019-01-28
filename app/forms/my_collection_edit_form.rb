b90bb886a0050080ecd44a227988f914b322f052
data-repo application owner <hydra@data-repo.dld.lib.vt.edu>
2019-01-28 15:26:04 +0000

    remove extra citation from form terms. Check for blank? not nil? because apparently it's empty string by default

--- app/forms/my_collection_edit_form.rb
+++ app/forms/my_collection_edit_form.rb
@@ -2,7 +2,5 @@ class MyCollectionEditForm < MyCollectionPresenter
   include HydraEditor::Form
   include HydraEditor::Form::Permissions
 
-  self.terms += [:citation]  
-
   self.required_fields = [:title]
 end
